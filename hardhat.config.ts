53ec2d4a815556e450f3c80a9e9383af6221e507
MarsIfeanyi <marschalice@gmail.com>
2023-10-09 15:28:07 +0100

    Add dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,6 @@ const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
 const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETH_MAINNET_RPC_URL = process.env.ETH_MAINNET_RPC_URL;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
